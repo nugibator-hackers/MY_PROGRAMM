708f592032a9ad05555d793d7ee220b4622529ae
nugibator-hackers <79287462+nugibator-hackers@users.noreply.github.com>
2024-01-07 23:46:07 +0300

    Update main.py
    Fix

--- main.py
+++ main.py
@@ -1,45 +1,23 @@
-# 104086280
-def is_interger(string: str) -> bool:
-    """Проверить что подстрока это число."""
-    flag: bool = True
-    try:
-        int(string)
-    except ValueError:
-        flag = False
-    return flag
+# 104349944
+def instructions(string: str):
+    stack = []
+    result: str = ''
+    number_in_string: int = 0
+    for character in string:
+        if character.isdigit():
+            number_in_string = number_in_string*10 + int(character)
 
-def repeater(string: str, count: int) -> str:
-    """Повтрорить строку count раз."""
-    return decoder(string) * (count - 1)
-
-
-def decoder(string: str) -> str:
-    """Декодировать строку."""
-    result = str()
-    value: int = 0 # Счетчик для умножения строки
-    pos_right: int = 0
-    for i in range(len(string)):
-        if string[i] == '[':
-            value_stack: int = 0 # Глубина скобок(нахождение парной скобки)
-            for j in range(i + 1, len(string)): # Цикл для нахождения парной скобки
-                if string[j] == '[': # Если встретили еще одну открывающую скобку глубина - 1 (внутреннего вырожения)
-                    value_stack += 1
-                if string[j] == ']':   # Если глубина равна нулю приравниваем правую позицию и выходим из цикла иначе глубина плюс один (вылезаем из внутреннего выражения)
-                    if value_stack == 0:
-                        pos_right = j
-                        break
-                    value_stack -= 1 
-            result += repeater(string[i + 1: pos_right], value) # Повторить выражение value раз
-            value = 0
-        elif is_interger(string[i]): # проверка что символ это цифра
-            value = value * 10 + int(string[i]) 
-        elif string[i] != ']': # Проверка что это обычный символ
-            result += string[i]
+        elif character == '[':
+            stack.append((number_in_string, result))
+            number_in_string = 0
+            result = ''
+        elif character == ']':
+            old_number_in_string, old_result = stack.pop()
+            result = old_result + result * old_number_in_string
+        else:
+            result += character
     return result
- 
-def main() -> None:
-    string = str(input())
-    print(decoder(string))
- 
-if __name__ == '__main__': 
-    main()
+
+
+if __name__ == '__main__':
+    print(instructions(input()))
